bc3b66df5ceb7045716d5ab047cd2dc04900cdf3
Fernanda Vincenzo <fernanda.vincenzo165@gmail.com>
2021-08-01 17:40:50 -0300

    exercicios

---
+++ 76-100/085.py
@@ -0,0 +1,12 @@
+numeros = [[], []]
+for c in range(1,8):
+    num = int(input(f'Digite um número para a posição {c}: '))
+    if num % 2 == 0:
+        numeros[0].append(num)
+    else:
+        numeros[1].append(num)
+
+numeros[0].sort()
+numeros[1].sort()
+print(f'Os números pares digitados foram: {numeros[0]}')
+print(f'Os números impares digitados foram: {numeros[1]}')
